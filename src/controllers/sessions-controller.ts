3f094800f9f20c01085d29d0dbfbea547c01f197
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-07-29 19:05:48 -0300

    feat: Create Session and Extract Token

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,29 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+import { authConfig } from "@/configs/auth"
+import { sign } from 'jsonwebtoken'
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: process.env.USER_NAME })
+    const { username, password } = request.body
+
+    const fakeUser = {
+      id: 1,
+      username: "rodrigo",
+      password: "123456",
+    }
+
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("Usuário e/ou senha incorreta!", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id),
+    })
+
+    return response.json({ token })
   }
 }
 
