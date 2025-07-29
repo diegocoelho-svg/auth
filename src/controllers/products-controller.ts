ddbd4aae825b94851c3a993d0571521222946639
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-07-28 23:04:06 -0300

    Initial Commit - Testing routes 🎉

---
+++ src/controllers/products-controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express"
+
+class ProductsController {
+  async index(request: Request, response: Response) {
+    return response.json({ message: "List of products" })
+  }
+
+  async create(request: Request, response: Response) {
+    return response.json({ message: "Created"})
+  }
+}
+
+export { ProductsController }
